987104a7406724a67c17806015680fbf0234ed8f
Sankarshan Kar <sankarshan.kar.sk@gmail.com>
2022-07-20 20:11:23 +0530

    Initial commit

---
+++ CNLab/Lab1/StructureAssign.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+ struct info 
+{
+    int roll_no;
+    char name[50];
+    float cgpa;
+};
+ void func(struct info *record)
+{
+    printf(" roll_no is: %d \n", record->roll_no);
+    printf(" Name is: %s \n", record->name);
+    printf(" Percentage is: %f \n", record->cgpa);
+} 
+int main() 
+{
+    struct info record;
+
+    record.roll_no=123;
+    strcpy(record.name, "asd");
+    record.cgpa = 7;
+ 
+    func(&record);
+    return 0;
+}
---
+++ CNLab/Lab1/VariableSwap.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+    int a = 10, b = 20;
+
+    printf("Before swapping: a = %d and b = %d\n", a, b);
+
+    swap(&a, &b);
+
+    printf("After swapping: a = %d and b = %d\n", a, b);
+
+    return 0;
+}
