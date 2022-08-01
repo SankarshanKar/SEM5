4c6c210835e61b2927e65cea2af5db9eec8f0996
Sankarshan Kar <sankarshan.kar.sk@gmail.com>
2022-08-01 19:54:24 +0530

    Added q1

---
+++ DAALab/Lab2/q1.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+void checkPrimeOne(int num)
+{
+    if(num == 0 || num == 1)
+    {
+        return;
+    }
+    int flag = 0;
+    int count = 0;
+    for(int i = 2; i < num; i++)
+    {
+        count++;
+        if(num % i == 0)
+        {
+            flag = 1;
+            cout<<num<<" is Non prime"<<endl;
+            break;
+        }
+    }
+    if(flag == 0)
+    {
+        cout<<num<<" is Prime"<<endl;
+    }
+    cout<<"No. of steps: "<<count<<endl;
+}
+
+void checkPrimeTwo(int num)
+{
+    if(num == 0 || num == 1)
+    {
+        return;
+    }
+    int flag = 0;
+    int count = 0;
+    for(int i = 2; i <= num / 2; i++)
+    {
+        count++;
+        if(num % i == 0)
+        {
+            flag = 1;
+            cout<<num<<" is Non prime"<<endl;
+            break;
+        }
+    }
+    if(flag == 0)
+    {
+        cout<<num<<" is Prime"<<endl;
+    }
+    cout<<"No. of steps: "<<count<<endl;
+
+}
+
+void checkPrimeThree(int num)
+{
+    if(num == 0 || num == 1)
+    {
+        return;
+    }
+    int flag = 0;
+    int count = 0;
+    for(int i = 2; i <= sqrt(num); i++)
+    {
+        count++;
+        if(num % i == 0)
+        {
+            flag = 1;
+            cout<<num<<" is Non prime"<<endl;
+            break;
+        }
+    }
+    if(flag == 0)
+    {
+        cout<<num<<" is Prime"<<endl;
+    }
+    cout<<"No. of steps: "<<count<<endl;
+}
+
+int main()
+{
+    int num;
+    cout<<"Enter number: ";
+    cin>>num;
+
+    checkPrimeOne(num);
+    checkPrimeTwo(num);
+    checkPrimeThree(num);
+
+    return 0;
+}
