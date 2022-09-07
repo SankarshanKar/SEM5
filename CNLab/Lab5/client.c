47f5a306c61fbd9c267abd0a6b45cda051ba2ed0
Sankarshan Kar <sankarshan.kar.sk@gmail.com>
2022-09-07 10:00:14 +0530

    Regular update

--- CNLab/Lab5/client.c
+++ CNLab/Lab5/client.c
@@ -11,27 +11,30 @@
 #define SERVERPORT 4952
 int main()
 {
-int sockfd;
-struct sockaddr_in their_addr;
-int numbytes;
-int arg[10];
-if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-perror("socket");
-exit(1);
-}
-their_addr.sin_family = AF_INET;
-their_addr.sin_port = htons(SERVERPORT);
-their_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-printf("Enter two numbers: \n");
-for(int i=0; i<2; i++){
-    scanf("%d", &arg[i]);
-}
-if ((numbytes = sendto(sockfd, arg, 10, 0,
-(struct sockaddr *)&their_addr, sizeof their_addr)) == -1) {
-perror("sendto");
-exit(1);
-}
-printf("sent %d bytes to %s\n", numbytes, inet_ntoa(their_addr.sin_addr));
-close(sockfd);
-return 0;
+    int sockfd;
+    struct sockaddr_in their_addr;
+    int numbytes;
+    int arg[10];
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+    {
+        perror("socket");
+        exit(1);
+    }
+    their_addr.sin_family = AF_INET;
+    their_addr.sin_port = htons(SERVERPORT);
+    their_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    printf("Enter two numbers: \n");
+    for (int i = 0; i < 2; i++)
+    {
+        scanf("%d", &arg[i]);
+    }
+    if ((numbytes = sendto(sockfd, arg, 10, 0,
+                           (struct sockaddr *)&their_addr, sizeof their_addr)) == -1)
+    {
+        perror("sendto");
+        exit(1);
+    }
+    printf("sent %d bytes to %s\n", numbytes, inet_ntoa(their_addr.sin_addr));
+    close(sockfd);
+    return 0;
 }
